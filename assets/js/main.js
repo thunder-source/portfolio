4bc2932f2da38e4814595a5f69a29df8e290b8db
thunder-source <majhipraditya@gmail.com>
2022-10-30 10:51:19 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,16 +38,17 @@ function scrollActive() {
   sections.forEach((current) => {
     const sectionHeight = current.offsetHeight;
     const sectionTop = current.offsetTop - 50;
-    sectionId = current.getAttribute("id");
-
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document
-        .querySelector(".nav__menu a[href*=" + sectionId + "]")
-        .classList.add("active-link");
-    } else {
-      document
-        .querySelector(".nav__menu a[href*=" + sectionId + "]")
-        .classList.remove("active-link");
+    const sectionId = current.getAttribute("id");
+    if (sectionId !== "project") {
+      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+        document
+          .querySelector(".nav__menu a[href*=" + sectionId + "]")
+          .classList.add("active-link");
+      } else {
+        document
+          .querySelector(".nav__menu a[href*=" + sectionId + "]")
+          .classList.remove("active-link");
+      }
     }
   });
 }
